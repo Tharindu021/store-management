9e25a48b65288482c7111c91cbbff0c8753b765b
tharindu021 <tharinduniroshan4@gmail.com>
2024-05-15 11:01:53 +0530

    data store function update

--- app/Http/Controllers/ProductImageController.php
+++ app/Http/Controllers/ProductImageController.php
@@ -3,50 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\DataResource;
-use App\Models\ProductImages;
-use domain\Facades\ImageFacade\ImageFacade;
 use domain\Facades\ProductImageFacade\ProductImageFacade;
-use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
-use Spatie\QueryBuilder\AllowedFilter;
-use Spatie\QueryBuilder\QueryBuilder;
+
 
 class ProductImageController extends Controller
 {
-    protected $productImage;
-
-    public function __construct()
-    {
-        $this->productImage = new ProductImages();
-    }
-
     public function store(Request $request, $product_id)
     {
-        $data = ($request->all());
-        
-        if (isset($data['images'])) {
-
-            $imageIdArray = [];
-            $storeImage = $data['images'];
-            foreach ($storeImage as $image) {
-                $imageId = ImageFacade::store($image);
-                $imageIdArray[] = $imageId;
-            };
-        return ProductImageFacade::store($imageIdArray, $product_id);
-
-        }
+        $data= $request->all();
+        return ProductImageFacade::store($data, $product_id);
     }
 
     public function getProducts($product_id)
     {
-        $productsData = ProductImageFacade::getProdoucts($product_id);
-        return DataResource::collection($productsData);
+        $productsdata = ProductImageFacade::getProdoucts($product_id);
+        return DataResource::collection($productsdata);
     }
 
     public function getImages($product_id)
     {
-        $productImage = ProductImageFacade::getImages($product_id);
-        return DataResource::collection($productImage);
+        $productimage = ProductImageFacade::getImages($product_id);
+        return DataResource::collection($productimage);
     }
 
     public function deleteImage($image_id)
--- domain/Services/ProductImageServices/ProductImageServices.php
+++ domain/Services/ProductImageServices/ProductImageServices.php
@@ -4,40 +4,49 @@ namespace domain\Services\ProductImageServices;
 
 use App\Http\Resources\DataResource;
 use App\Models\ProductImages;
+use domain\Facades\ImageFacade\ImageFacade;
 use Illuminate\Support\Facades\Storage;
 
 
 class ProductImageServices
 {
-    protected $productImage;
+    protected $productimage;
 
     public function __construct()
     {
-        $this->productImage = new ProductImages();
+        $this->productimage = new ProductImages();
     }
 
-    public function store($image_ids, $product_id)
+    public function store($data, $product_id)
     {
-        foreach ($image_ids as $data) {
-            $this->productImage->create([
-                'product_id' => $product_id,
-                'image_id' => $data,
-            ]);
+        if (isset($data['images'])) {
+
+            $storeimage = $data['images'];
+            foreach ($storeimage as $image) {
+
+                $imageid = ImageFacade::store($image);
+
+                $this->productimage->create([
+                    'product_id' => $product_id,
+                    'image_id' => $imageid,
+                ]);
+                
+            };
         }
     }
 
     public function getProdoucts($id)
     {
-        $productImages = $this->productImage->where('product_id', $id)->get();
-        return  $productImages;
+        $productimages = $this->productimage->where('product_id', $id)->get();
+        return  $productimages;
     }
 
     public function getImages($id)
     {
-        $productImages = $this->productImage->where('product_id', $id)->get();
+        $productimages = $this->productimage->where('product_id', $id)->get();
         $imageNames = [];
-        foreach ($productImages as $productImage) {
-            $image = $productImage->images()->first();
+        foreach ($productimages as $productimage) {
+            $image = $productimage->images()->first();
             if ($image) {
                 $imageNames[] = $image;
             }
@@ -52,17 +61,17 @@ class ProductImageServices
 
     public function update($id)
     {
-        $this->productImage->where('status', 0)->update(['status' => 1]);
-        return $this->productImage->where('image_id', $id)->update(['status' => 0]);
+        $this->productimage->where('status', 0)->update(['status' => 1]);
+        return $this->productimage->where('image_id', $id)->update(['status' => 0]);
     }
 
     public function deactive($id)
     {
-        return $this->productImage->where('image_id', $id)->update(['status' => 2]);
+        return $this->productimage->where('image_id', $id)->update(['status' => 2]);
     }
 
     public function active($id)
     {
-        return $this->productImage->where('image_id', $id)->update(['status' => 1]);
+        return $this->productimage->where('image_id', $id)->update(['status' => 1]);
     }
 }
